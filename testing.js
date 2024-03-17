cfa89bcc71e42812130f185574693b9b21cebc20
dallinsiddoway <62368771+dallinsiddoway@users.noreply.github.com>
2024-03-16 20:32:59 -0600

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Testing
+// @namespace    Coool stuff
+// @version      2024-03-16
+// @description  Alerts
+// @author       You
+// @match *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+alert('hi')
+
